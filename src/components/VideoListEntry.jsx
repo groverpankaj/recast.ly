9ed59ad0a813981d2c67914fe3b10395b7b30102
groverpankaj <pgrover2079@gmail.com>
2019-12-31 11:52:47 -0800

    updates the current video when the video title is clicked v2

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -5,7 +5,7 @@ var VideoListEntry = (props) => (
     </div>
     <div className="media-body">
 
-      <div className="video-list-entry-title" onClick={function() {props.updateCurrentVideoHandler(props.video);}}>{props.video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={function() { props.updateCurrentVideoHandler(props.video); }}>{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>

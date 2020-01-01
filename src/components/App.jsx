0ed921c422a9d20cebf20f6c8fea638419a480af
groverpankaj <pgrover2079@gmail.com>
2019-12-31 17:58:02 -0800

    search field is  working via submit button. next step is to make the list update as user types in the search field.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import ExampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
 import SearchYouTube from '../lib/searchYouTube.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import Search from './Search.js';
 
 // var App = () => (
 //   <div>
@@ -27,27 +28,49 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchOptions: {
+        query: 'javascript',
+        max: 7 || 5,
+        key: YOUTUBE_API_KEY
+      },
       // videoData: ExampleVideoData,
       currentVideo: ExampleVideoData[0],
       videoData: [],
       // currentVideo: {}
     };
+
+    this.updateVideoDataInit = this.updateVideoDataInit.bind(this);
     this.updateCurrentVideo = this.updateCurrentVideo.bind(this);
     this.updateVideoData = this.updateVideoData.bind(this);
+    this.searchSubmit = this.searchSubmit.bind(this);
   }
 
+
+
   componentDidMount() {
-    this.getVideoData();
+    this.getVideoDataInit();
+  }
+
+  getVideoDataInit() {
+    SearchYouTube(this.state.searchOptions, this.updateVideoDataInit);
   }
 
   getVideoData() {
-    SearchYouTube(searchOptions, this.updateVideoData);
+    SearchYouTube(this.state.searchOptions, this.updateVideoData);
   }
 
   updateVideoData(inputData) {
-    console.log(inputData);
+    // console.log(inputData);
     this.setState({
       videoData: inputData
+    });
+  }
+
+  updateVideoDataInit(inputData) {
+    // console.log(inputData);
+    this.setState({
+      videoData: inputData,
+      currentVideo: inputData[0]
     });
   }
 
@@ -57,14 +80,26 @@ class App extends React.Component {
     });
   }
 
+  searchSubmit(query) {
 
+    this.setState({
+      searchOptions: {
+        query: query,
+        max: 7 || 5,
+        key: YOUTUBE_API_KEY
+      }
+    }, () => this.getVideoData()
+    );
+
+  }
 
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            {/* <div><h5><em>search</em> view goes here</h5></div> */}
+            <Search searchSubmit={this.searchSubmit}></Search>
           </div>
         </nav>
         <div className="row">
@@ -82,11 +117,5 @@ class App extends React.Component {
     );
   }
 }
-
-var searchOptions = {
-  query: 'javascript',
-  max: 7 || 5,
-  key: YOUTUBE_API_KEY
-};
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,10 @@
-var Search = () => (
+var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
+    <input className="form-control" type="text" id="searchField"/>
+    <button className="btn hidden-sm-down" onClick={ function() {
+      let queryText = document.getElementById('searchField').value;
+      props.searchSubmit(queryText);
+    } }>
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div>
